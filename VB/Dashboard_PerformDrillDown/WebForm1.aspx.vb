e26402780e7a7099ca4fa221b27c0d119e26565c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Dashboard_PerformDrillDown/WebForm1.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DashboardWeb
+
+Namespace Dashboard_PerformDrillDown
+    Partial Public Class WebForm1
+        Inherits System.Web.UI.Page
+
+        Protected Sub ASPxDashboardViewer1_ConfigureDataConnection(ByVal sender As Object, ByVal e As ConfigureDataConnectionWebEventArgs)
+            If e.ConnectionName = "nwindConnection" Then
+
+                ' Gets connection parameters used to establish a connection to the database.
+                Dim parameters As Access97ConnectionParameters = CType(e.ConnectionParameters, Access97ConnectionParameters)
+                Dim databasePath As String = Server.MapPath("App_Data/nwind.mdb")
+
+                ' Specifies the path to a database file.                    
+                parameters.FileName = databasePath
+            End If
+        End Sub
+    End Class
+End Namespace
